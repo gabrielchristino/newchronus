5b1e9f3d6cbf86c35b754e1ff8eaf3234fae840d
Gabriel Christino <gabriel.christino@gmail.com>
2024-05-31 15:27:05 -0300

    "Simplified isBissexto function implementation"

--- src/clock.cpp
+++ src/clock.cpp
@@ -35,28 +35,7 @@ String getSegundo() { return String(segundo); }
 
 uint16_t isBissexto(uint16_t ano)
 {
-    if (ano % 4 == 0)
-    {
-        if (ano % 100 == 0)
-        {
-            if (ano % 400 == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return true;
-        }
-    }
-    else
-    {
-        return false;
-    }
+    return ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0);
 }
 
 void setDia(int8_t valor)
