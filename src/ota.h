66ce341dc5b99d2fab044e0844a28df8c8e5c508
Gabriel Christino <gabriel.christino@gmail.com>
2024-07-05 14:51:35 -0300

    "Added OTA update functionality, modified .gitignore file, and updated ota.cpp and ota.h files"

--- src/ota.h
+++ src/ota.h
@@ -1,3 +1,21 @@
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include "Update.h"
+#include <WiFiClientSecure.h>
+
+// Define WiFi credentials
+#define ssid "SSID"
+#define password "PASSWORD"
+
+// Define server details and file path
+#define HOST "raw.githubusercontent.com"
+#define PATH "/gabrielchristino/newchronus/develop/.pio/build/esp32doit-devkit-v1/firmware.bin"
+#define PORT 443
+
+// Define the name for the downloaded firmware file
+#define FILE_NAME "firmware.bin"
+
 void createAP();
 void updateCodeOta();
 void otaHandle();
+void performOTAUpdateFromSPIFFS();
