4b2d7ec06cb4f108874d15a02fce5d45b5450d65
Gabriel Christino <gabriel.christino@gmail.com>
2024-07-08 15:48:37 -0300

    "Refactored OTA update code to stream firmware data from HTTP response instead of saving to SPIFFS"

--- src/ota.cpp
+++ src/ota.cpp
@@ -22,75 +22,41 @@ void updateCodeOta()
 {
   if (getConfig("ota") == "1")
   {
-    File file = SPIFFS.open("/firmware.bin");
-    if (file)
-    {
-      Serial.println("delete file");
-      SPIFFS.remove("/firmware.bin");
-    }
-
-    file.close();
-
     HTTPClient http;
     http.begin(PATH);
     int httpCode = http.GET();
 
     if (httpCode == HTTP_CODE_OK)
     {
-      String firmwareData = http.getString();
-      file = SPIFFS.open("/firmware.bin", FILE_WRITE);
-      if (!file)
-      {
-        SPIFFS.remove("/firmware.bin");
-        Serial.println("Failed to open file for writing");
-        return;
-      }
-      file.print(firmwareData);
-      file.close();
       Serial.println("Firmware downloaded and saved to SPIFFS");
-
-      file = SPIFFS.open("/firmware.bin");
-
-      if (!file)
-      {
-        saveConfig("ota", "0");
-        SPIFFS.remove("/firmware.bin");
-        Serial.println("Failed to open file for reading");
-        return;
-      }
 
       Serial.println("Starting update..");
 
-      size_t fileSize = file.size();
+      size_t fileSize = http.getString().length();
       Serial.println(fileSize);
 
       if (!Update.begin(UPDATE_SIZE_UNKNOWN))
       {
         saveConfig("ota", "0");
-        SPIFFS.remove("/firmware.bin");
         Serial.println("Cannot do the update");
         return;
       };
-
-      Update.write(file);
+  
+      Update.writeStream(http.getStream());
 
       if (Update.end())
       {
-        SPIFFS.remove("/firmware.bin");
         Serial.println("Successful update");
       }
       else
       {
         saveConfig("ota", "0");
-        SPIFFS.remove("/firmware.bin");
         Serial.println("Error Occurred: " + String(Update.getError()));
         return;
       }
 
-      file.close();
 
       saveConfig("ota", "0");
-      SPIFFS.remove("/firmware.bin");
 
       Serial.println("Reset in 4 seconds...");
       delay(4000);
