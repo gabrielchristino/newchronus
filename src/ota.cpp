a2e541c923c35c983ac619dbfe199fcd32b51290
Gabriel Christino <gabriel.christino@gmail.com>
2024-07-08 13:25:25 -0300

    "Updated firmware.bin, partitions.csv, and ota.cpp files"

--- src/ota.cpp
+++ src/ota.cpp
@@ -12,6 +12,7 @@ void createAP()
     while (WiFi.status() != WL_CONNECTED)
     {
       delay(500);
+      Serial.print(".");
     }
     wellcome();
   }
@@ -21,28 +22,37 @@ void updateCodeOta()
 {
   if (getConfig("ota") == "1")
   {
-    SPIFFS.remove("/firmware.bin");
-    HTTPClient http;
-    http.begin(PATH);
-    int httpCode = http.GET();
+    File file = SPIFFS.open("/firmware.bin");
+    // if (file)
+    // {
+    //   Serial.println("delete file");
+    //   SPIFFS.remove("/firmware.bin");
+    // }
 
-    if (httpCode == HTTP_CODE_OK)
-    {
-      String firmwareData = http.getString();
-      File file = SPIFFS.open("/firmware.bin", FILE_WRITE);
+    // file.close();
+
+    // HTTPClient http;
+    // http.begin(PATH);
+    // int httpCode = http.GET();
+
+    // if (httpCode == HTTP_CODE_OK)
+    // {
+    //   String firmwareData = http.getString();
+    //   file = SPIFFS.open("/firmware.bin", FILE_WRITE);
+    //   if (!file)
+    //   {
+    //     Serial.println("Failed to open file for writing");
+    //     return;
+    //   }
+    //   file.print(firmwareData);
+    //   file.close();
+    //   Serial.println("Firmware downloaded and saved to SPIFFS");
+
+    //   file = SPIFFS.open("/firmware.bin");
+
       if (!file)
       {
-        Serial.println("Failed to open file for writing");
-        return;
-      }
-      file.print(firmwareData);
-      // file.close();
-      Serial.println("Firmware downloaded and saved to SPIFFS");
-
-      // file = SPIFFS.open("/firmware.bin");
-
-      if (!file)
-      {
+        saveConfig("ota", "0");
         Serial.println("Failed to open file for reading");
         return;
       }
@@ -54,6 +64,7 @@ void updateCodeOta()
 
       if (!Update.begin(fileSize))
       {
+        saveConfig("ota", "0");
         Serial.println("Cannot do the update");
         return;
       };
@@ -66,22 +77,25 @@ void updateCodeOta()
       }
       else
       {
+        saveConfig("ota", "0");
         Serial.println("Error Occurred: " + String(Update.getError()));
         return;
       }
 
       file.close();
-      
+
+      saveConfig("ota", "0");
+
       Serial.println("Reset in 4 seconds...");
       delay(4000);
 
       ESP.restart();
-    }
-    else
-    {
-      Serial.println("Error downloading firmware");
-    }
-    http.end();
+    // }
+    // else
+    // {
+    //   Serial.println("Error downloading firmware");
+    // }
+    // http.end();
   }
 }
 
